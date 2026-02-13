e9657968799c480c79ff6760f69cdb475bd67c45
Eason-F <eason.ycfeng@outlook.com>
2026-02-13 20:29:38 +0800

    added util file and implemented moveInDirection function for drive

--- src/drive/motor/Motor.cpp
+++ src/drive/motor/Motor.cpp
@@ -2,7 +2,13 @@
 
 #include <drive/motor/Motor.hpp>
 
-Motor::Motor(const int pwmPin, const int directionPin, const int pulsePin) {
+Motor::Motor() {
+    this-> pwmPin = 99;
+    this-> directionPin = 99;
+    this-> pulsePin = 99;
+}
+
+Motor::Motor(const int &pwmPin, const int &directionPin, const int &pulsePin) {
     this-> pwmPin = pwmPin;
     this-> directionPin = directionPin;
     this-> pulsePin = pulsePin;
@@ -16,8 +22,8 @@ void Motor::incrementPulseCount() {
     pulseCount++;
 }
 
-void Motor::setMotorSpeed(int speed, enum MotorDirection direction) {
+void Motor::setMotorSpeed(int speed) {
     speed = 255 - (speed / 100) * 255;
-    digitalWrite(directionPin, direction);
+    digitalWrite(directionPin, (speed > 0) ? HIGH : LOW);
     analogWrite(pwmPin, speed);
 }
