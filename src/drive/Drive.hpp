e2f4cd16bcd54f5b90d62dae9c83a0d50e33af5d
Eason-F <eason.ycfeng@outlook.com>
2026-04-06 12:04:03 +0800

    fix: update pin numbers for motors

--- src/drive/Drive.hpp
+++ src/drive/Drive.hpp
@@ -19,30 +19,30 @@ class Drive {
         void turnInDirection(int direction, int speed);
 
     private: 
-        const int STBY_PIN_1 = 99;
-        const int STBY_PIN_2 = 99;
+        const int STBY_PIN_1 = 8;
+        const int STBY_PIN_2 = 15;
 
-        const int PWM_PIN_1 = 99;
-        const int DIRECTION_PIN1_1 = 99;
-        const int DIRECTION_PIN2_1 = 99;
+        const int PWM_PIN_1 = 2;
+        const int DIRECTION_PIN1_1 = 3;
+        const int DIRECTION_PIN2_1 = 4;
         const int ENCODER_PIN1_1 = 99;
         const int ENCODER_PIN2_1 = 99;
 
-        const int PWM_PIN_2 = 99;
-        const int DIRECTION_PIN1_2 = 99;
-        const int DIRECTION_PIN2_2 = 99;
+        const int PWM_PIN_2 = 5;
+        const int DIRECTION_PIN1_2 = 6;
+        const int DIRECTION_PIN2_2 = 7;
         const int ENCODER_PIN1_2 = 99;
         const int ENCODER_PIN2_2 = 99;
 
-        const int PWM_PIN_3 = 99;
-        const int DIRECTION_PIN1_3 = 99;
-        const int DIRECTION_PIN2_3 = 99;
+        const int PWM_PIN_3 = 9;
+        const int DIRECTION_PIN1_3 = 10;
+        const int DIRECTION_PIN2_3 = 11;
         const int ENCODER_PIN1_3 = 99;
         const int ENCODER_PIN2_3 = 99;
 
-        const int PWM_PIN_4 = 99;
-        const int DIRECTION_PIN1_4 = 99;
-        const int DIRECTION_PIN2_4 = 99;
+        const int PWM_PIN_4 = 20;
+        const int DIRECTION_PIN1_4 = 21;
+        const int DIRECTION_PIN2_4 = 22;
         const int ENCODER_PIN1_4 = 99;
         const int ENCODER_PIN2_4 = 99;
 
