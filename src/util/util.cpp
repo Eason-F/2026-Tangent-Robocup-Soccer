ae5d5492486deb2bafc080cf6c295938de29e06d
Eason-F <eason.ycfeng@outlook.com>
2026-04-20 17:53:52 +0800

    feature: create vector class

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -1,24 +1,42 @@
 #include <util/util.hpp>
 
-PIDController::PIDValues::PIDValues(const float &kP, const float &kI, const float &kD) {
+PIDController::PIDController(const float &kP, const float &kI, const float &kD) {
     this-> kP = kP;
     this-> kI = kI;
     this-> kD = kD;
-}
-
-PIDController::PIDController() {};
-
-PIDController::PIDController(const float &kP, const float &kI, const float &kD) {
-    pidValues.kP = kP;
-    pidValues.kI = kI;
-    pidValues.kD = kD;
-};
-
-PIDController::PIDController(PIDValues pidValues) {
-    this-> pidValues = pidValues;
 };
 
 float PIDController::adjustmentValue(float current, float target) {
     float error = target - current;
-    return error * pidValues.kP;
+    return error * kP;
 };
+
+Vector::Vector(Position, const float &posX, const float &posY) {
+    this-> x = posX;
+    this-> y = posY;
+    angle = acos(posY / posX);
+    length = sqrt(pow(posX, 2) + pow(posY, 2));
+}
+
+Vector::Vector(AngDir, const float &angle, const float &length) {
+    this-> angle = angle;
+    this-> length = length;
+    x = length * cos(angle);
+    y = length * sin(angle);
+}
+
+Vector Vector::operator+(const Vector &vec) {
+    return Vector(Position {}, x + vec.x, y + vec.y);
+}
+
+Vector Vector::operator-(const Vector &vec) {
+    return Vector(Position {}, x - vec.x, y - vec.y);
+}
+
+Vector Vector::operator*(const float &n) {
+    return Vector(Position {}, x * n, y * n);
+}
+
+Vector Vector::operator/(const float &n) {
+    return Vector(Position {}, x / n, y / n);
+}
--- src/util/util.hpp
+++ src/util/util.hpp
@@ -2,28 +2,32 @@
 
 #include <Arduino.h>
 
-class Direction {
-    public:
-        const static int CLOCKWISE = 1;
-        const static int ANTICLOCKWISE = -1;
-
-};
-
 class PIDController {
     private:
-        struct PIDValues {
-            float kP;
-            float kI;
-            float kD;
+        float kP;
+        float kI;
+        float kD;
 
-            PIDValues(const float &kP, const float &kI, const float &kD);
-        };
     public:
-        PIDValues pidValues = PIDValues(0, 0, 0);
-        
         PIDController();
         PIDController(const float &kP, const float &kI, const float &kD);
-        PIDController(PIDValues pidValues);
 
         float adjustmentValue(float target, float error);
+};
+
+class Vector {
+    float x;
+    float y;
+    float angle;
+    float length;
+
+    struct Position{}; // structs for constructor differentiation
+    struct AngDir{};
+    Vector(Position, const float &posX, const float &posY);
+    Vector(AngDir, const float &angle, const float &length);
+
+    Vector operator+(const Vector &vec);
+    Vector operator-(const Vector &vec);
+    Vector operator*(const float &n);
+    Vector operator/(const float &n);
 };
