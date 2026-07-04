a8c9658a4d8f35f5b9795b4ead1fc3cd55e1cc23
Eason-F <eason.ycfeng@outlook.com>
2026-07-04 15:20:31 +0800

    fix: update progam for odometry

--- src/odometry/Odometry.hpp
+++ src/odometry/Odometry.hpp
@@ -1,17 +1,23 @@
 #include <SparkFun_Qwiic_OTOS_Arduino_Library.h>
 #include <Wire.h>
 
+#include <util/util.hpp>
+
 class OpticalOdometry {
     public:
         OpticalOdometry(TwoWire &wirePort);
 
-        float getX();
-        float getY();
-        void updatePosition();
+        void setup();
+        void update();
         void resetPosition();
         void setPosition(sfe_otos_pose2d_t &pose);
 
+        float getX();
+        float getY();
+        float getHeading();
+
     private:
+        TwoWire &wirePort;
         QwiicOTOS odometrySensor;
         sfe_otos_pose2d_t position;
 };
