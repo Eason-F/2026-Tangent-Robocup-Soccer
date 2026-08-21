0a2622281ebff76d25a6b032c820171e99daebd8
Eason-F <eason.ycfeng@outlook.com>
2026-08-21 22:11:05 +0800

    fix: tune orbit further

--- src/Robot.hpp
+++ src/Robot.hpp
@@ -47,38 +47,43 @@ class Robot {
         static constexpr uint8_t APPROACH_SPD = 150;
 
         static constexpr uint8_t ORBIT_APPROACH_SPD = 120;
-        static constexpr uint8_t ORBIT_SPD = 80;
-        static constexpr uint8_t ORBIT_DISTANCE = 85;
-        static constexpr uint8_t ORBIT_ENTRY_TOLERANCE = 5;
+        static constexpr uint8_t ORBIT_SPD = 160;
+        static constexpr uint8_t ORBIT_DISTANCE = 87;
+        static constexpr uint8_t ORBIT_ENTRY_TOLERANCE = 10;
         static constexpr uint8_t ORBIT_EXIT_TOLERANCE = 15;
-        static constexpr uint8_t ORBIT_DEBOUNCE_MS = 50;
+        static constexpr uint8_t ORBIT_DEBOUNCE_MS = 100;
         unsigned long accumulatedOrbitTime = 0;
 
-        static constexpr uint8_t CAPTURED_SPD = 200;
+        static constexpr uint8_t CAPTURED_MAX_SPD = 250;
+        static constexpr uint8_t CAPTURED_MIN_SPD = 180;
         static constexpr uint8_t ENTER_ALIGNMENT_TOLERANCE = 10;
-        static constexpr uint8_t EXIT_ALIGNMENT_TOLERANCE = 30;
-        static constexpr uint8_t ALIGNED_DEBOUNCE_MS = 50;
+        static constexpr uint8_t EXIT_ALIGNMENT_TOLERANCE = 25;
+        static constexpr uint8_t ALIGNED_DEBOUNCE_MS = 0;
+        static constexpr uint16_t SPEED_RAMP_MAX_MS = 700;
         unsigned long accumulatedAlignedTime = 0;
 
         PIDController approachPID = PIDController(0.5, 0, 0, 0.0, 1.0);
-        PIDController orbitTangentPID = PIDController(0.2, 0, 0.001, -1.0, 1.0);
-        PIDController orbitDistancePID = PIDController(0.3, 0, 0.001, -1.0, 1.0);
+        PIDController orbitTangentPID = PIDController(0.04, 0, 0.001, -1.0, 1.0);
+        PIDController orbitDistancePID = PIDController(0.3, 0, 0.001, -0.1, 1.0);
 
         static constexpr uint8_t TURN_SPD = 100;
         static constexpr uint8_t HEADING_TOLERANCE = 15;
+        static constexpr uint8_t BALL_TILT_RANGE = 70;
         PIDController headingPID = PIDController(0.015, 0.001, 0.0, -1.0, 1.0);
 
         static constexpr uint16_t ESCAPE_DURATION = 100;
-        float escapeDirection = 0.0f;
         elapsedMillis elapsedEscapeTime = ESCAPE_DURATION;
+        float escapeDirection = 0.0f;
 
         elapsedMillis elapsedLastTime;
         State robotState = State::SEARCH;
+        float targetHeading;
 
         bool handleEdgeDetection(const float dt);
         void handleHeadingCorrection(const float dt, const float targetHeading);
-        void maneuverAroundBall(const float dt, const float targetBallHeading);
+        void handleTargetHeading();
         void checkRobotState(const float dt, const float targetBallHeading);
+        void maneuverAroundBall(const float dt, const float targetBallHeading);
 
         Button button;
         UartIRSensor irSensor;
